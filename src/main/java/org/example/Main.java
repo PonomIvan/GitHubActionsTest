f1fa5989aa94948c34bbd74d440a7020d543d4ff
IvanPonoma <vanecchhka@mail.ru>
2024-11-17 02:51:05 +0800

    11.Code with error

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 5/0;
+        int i = 5/0
             System.out.println("i = " + i);
     }
 }
